d9e3373ae43608f94906d64016ea158e3c7eda09
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-09-26 23:59:04 +1000

    feat: copy across base eslint config

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        trailingComma: "all",
+      },
+    ],
+    "no-useless-rename": 2,
+    "import/order": "error",
+  },
+};
