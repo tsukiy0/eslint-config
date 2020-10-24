6521fca34e782e71f18d634f6d9ff5343508c4ae
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-10-24 20:01:45 +1100

    ci: use semantic-release for package releases

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -2,10 +2,9 @@
 
 set -euxo pipefail
 
-npm set //npm.pkg.github.com/:_authToken ${GITHUB_TOKEN}
-npm publish --access public --dry-run
+yarn deploy --dry-run
 
 if [ ${DEPLOY:-"false"} == "true" ]
 then
-    npm publish --access public
+    yarn deploy
 fi
